cbc62d8e26e5ce211b2e6a817bca72a92f8997a1
kauefariass <95444539+kauefariass@users.noreply.github.com>
2022-05-27 01:33:30 -0300

    funcionamento ok

--- cronometro.js
+++ cronometro.js
@@ -1,10 +1,12 @@
 window.onload = function(){
+    let Minutos = 00;
     let Segundos = 00;
     let Dezenas = 00;
     let AcrecentarDezenas = document.getElementById("dezenas");
     let AcrecentarSegundos = document.getElementById("segundos");
+    let AcrecentarMinutos = document.getElementById("minutos");
     let comecar = document.getElementById("comecar");
-    let parar = documet.getElementById("parar");
+    let parar = document.getElementById("parar");
     let reset = document.getElementById("resetar");
     let intervalo;
 
@@ -20,8 +22,10 @@ window.onload = function(){
         clearInterval(intervalo);
         Dezenas = "00";
         Segundos = "00";
+        Minutos = "00";
         AcrecentarDezenas.innerHTML = Dezenas;
         AcrecentarSegundos.innerHTML = Segundos;
+        AcrecentarMinutos.innerHTML = Minutos;
     }
 
     function ComecarTimer(){
@@ -35,15 +39,31 @@ window.onload = function(){
             AcrecentarDezenas.innerHTML = Dezenas;
         }
 
-        if(Dezenas > 99){
+        if(Dezenas > 60){
             Segundos++;
             AcrecentarSegundos.innerHTML = "0" + Segundos;
             Dezenas = 0;
             AcrecentarDezenas.innerHTML = "0" + 0;
         }
 
-        if(Segundos > 9){
-            AcrecentarSegundos.innerHTML + Segundos;
+        if(Segundos <= 9){
+            AcrecentarSegundos.innerHTML = "0" + Segundos;
         }
+
+        if(Dezenas > 9){
+            AcrecentarSegundos.innerHTML = Segundos;
+        }
+
+        if(Segundos > 60){
+            Minutos++;
+            AcrecentarMinutos.innerHTML = "0" + Minutos;
+            Segundos = 0;
+            AcrecentarSegundos.innerHTML = "0" + 0;
+        }
+
+        if(Minutos > 9){
+            AcrecentarMinutos.innerHTML + Minutos;
+          }
+
     }
 }
